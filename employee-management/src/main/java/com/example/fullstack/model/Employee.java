c271cf5a935138c2b421872ac3c7519ec61d2a26
satishht <satish246.ht@gmail.com>
2023-06-14 00:54:55 +0530

    Create a proper folder Structure

---
+++ employee-management/src/main/java/com/example/fullstack/model/Employee.java
@@ -0,0 +1,60 @@
+package com.example.fullstack.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(long id, String firstName, String lastName, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
